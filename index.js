90841371ddeb02f58d493a1ee9ca91e0f666db3b
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-07-31 16:03:24 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(someValue) {
+    if (someValue > 42) {
+        return someValue - 42
+    }
+    else if (someValue < 42) {
+        return 42 - someValue
+    }
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264
+    }
+    else if (start < destination) {
+        return (destination - start) * 264
+    }
+
+
+
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start, destination) <= 2000) {
+        return + ((distanceTravelledInFeet(start, destination)) * .02 - 8).toFixed(2);
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return "cannot travel that far"
+    }
+}
